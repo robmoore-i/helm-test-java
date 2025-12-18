d46c4be3af4bcc1e49ebaf2d87192e06a62207a7
Rob Moore <robmoore121@gmail.com>
2025-12-18 15:39:06 +0800

    Build logic for downloading and using Helm executable

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -8,6 +8,8 @@ plugins {
 }
 
 repositories {
+    mavenCentral()
+
     maven {
         name = "Central Portal Snapshots"
         url = URI.create("https://central.sonatype.com/repository/maven-snapshots")
@@ -16,7 +18,47 @@ repositories {
         }
     }
 
-    mavenCentral()
+    exclusiveContent {
+        forRepository {
+            ivy {
+                url = uri("https://get.helm.sh")
+                patternLayout { artifact("[artifact]-v[revision]-[classifier].[ext]") }
+                metadataSources {
+                    artifact()
+                }
+            }
+        }
+        filter {
+            includeGroup("io.github.helm")
+        }
+    }
+}
+
+val helmExecutable by configurations.creating {
+    isCanBeResolved = true
+    isCanBeConsumed = false
+}
+
+val helmPlatform = "darwin-amd64"
+val helmVersion = "3.19.4"
+
+dependencies {
+    helmExecutable("io.github.helm:helm:$helmVersion:$helmPlatform@tar.gz")
+}
+
+val expandHelmExecutable by tasks.registering(Sync::class) {
+    from(tarTree(helmExecutable.singleFile))
+    into(layout.buildDirectory.dir("helm/executable/$helmPlatform-$helmVersion"))
+}
+
+class FileArgumentProvider(
+    @Input val propertyName: String,
+    @PathSensitive(PathSensitivity.NONE) @InputFile val inputFile: Provider<File>
+) : CommandLineArgumentProvider {
+
+    override fun asArguments(): Iterable<String?> {
+        return listOf("-D$propertyName=${inputFile.get().path}")
+    }
 }
 
 testing {
@@ -25,6 +67,16 @@ testing {
             useJUnitJupiter()
             dependencies {
                 implementation("com.rrmoore:helm-test-java:0.1.0-SNAPSHOT")
+            }
+            targets {
+                all {
+                    testTask {
+                        jvmArgumentProviders += FileArgumentProvider(
+                            "com.rrmoore.helm.test.executable.path",
+                            expandHelmExecutable.map { it.destinationDir.resolve("$helmPlatform/helm") }
+                        )
+                    }
+                }
             }
         }
     }
