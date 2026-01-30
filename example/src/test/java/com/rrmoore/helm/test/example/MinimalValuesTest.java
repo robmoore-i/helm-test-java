18b8104334048639295cceca46546559b6319142
Rob Moore <robmoore121@gmail.com>
2026-01-30 13:00:54 +0800

    Add example usage for verifyChecksumAnnotations

--- example/src/test/java/com/rrmoore/helm/test/example/MinimalValuesTest.java
+++ example/src/test/java/com/rrmoore/helm/test/example/MinimalValuesTest.java
@@ -1,7 +1,11 @@
 package com.rrmoore.helm.test.example;
 
 import com.rrmoore.helm.test.HelmExecutor;
+import com.rrmoore.helm.test.Workload;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MinimalValuesTest {
 
@@ -10,5 +14,18 @@ public class MinimalValuesTest {
     @Test
     void rendersSuccessfullyWithNoValues() {
         helm.template();
+    }
+
+    @Test
+    void everyWorkloadHasChecksumAnnotationsForItsResources() {
+        var manifests = helm.template();
+
+        var failures = manifests.findAllWorkloads().stream()
+            .map(Workload::verifyChecksumAnnotations)
+            .filter(it -> !it.success())
+            .map(Workload.VerifyChecksumAnnotationsResult::message)
+            .collect(Collectors.joining("\n"));
+
+        assertTrue(failures.isBlank(), failures);
     }
 }
